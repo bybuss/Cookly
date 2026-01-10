46717c34880f663079ab47c2fea347463b6cfb4e
bybuss <kukuhaha729@gmail.com>
2026-01-10 20:23:53 +0300

    added: variation of several meals in row

--- app/src/main/java/bob/colbaskin/ufood/home/presentation/components/MealsCardRow.kt
+++ app/src/main/java/bob/colbaskin/ufood/home/presentation/components/MealsCardRow.kt
@@ -1,31 +1,41 @@
 package bob.colbaskin.ufood.home.presentation.components
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import bob.colbaskin.ufood.common.design_system.theme.UfoodTheme
 
 @Composable
-fun MealsCardRow(modifier: Modifier = Modifier) {
-    Row(
+fun MealsCardRow(modifier: Modifier = Modifier, mealsList: List<MealType>) {
+    LazyRow (
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
+        horizontalArrangement = Arrangement.spacedBy(25.dp),
         modifier = modifier.fillMaxWidth()
     ) {
-        MealsCard(mealType = MealType.BREAKFAST)
-        MealsCard(mealType = MealType.LUNCH)
-        MealsCard(mealType = MealType.DINNER)
+        mealsList.forEach { mealType ->
+            item {
+                MealsCard(mealType = mealType)
+            }
+        }
     }
 }
 
 @Preview
 @Composable
 fun MealsCardRowPreview() {
+    val mealsList = listOf(
+        MealType.BREAKFAST,
+        MealType.LUNCH,
+        MealType.DINNER,
+        MealType.DINNER,
+    )
+
     UfoodTheme {
-        MealsCardRow()
+        MealsCardRow(mealsList = mealsList)
     }
 }
