91913e016f04868a9e61c7d7256a7efba48c0b6e
bybuss <kukuhaha729@gmail.com>
2026-01-17 15:10:51 +0300

    added: more info in DishOfTheDayCard

--- app/src/main/java/bob/colbaskin/ufood/home/presentation/components/DishOfTheDay.kt
+++ app/src/main/java/bob/colbaskin/ufood/home/presentation/components/DishOfTheDay.kt
@@ -1,8 +1,11 @@
 package bob.colbaskin.ufood.home.presentation.components
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -23,6 +27,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,11 +51,37 @@ fun DishOfTheDay(modifier: Modifier = Modifier) {
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(8.dp))
+        DishOfDayCard(
+            name = "Fried Shrimp",
+            backgroundImage = R.drawable.shrimp_soup_image,
+            rating = 4.8,
+            ratingAmount = 163,
+            minutes = 20,
+            kcal = 150,
+            aiAvatar = R.drawable.ai_avatar
+        )
+    }
+}
 
+@Composable
+private fun DishOfDayCard(
+    modifier: Modifier = Modifier,
+    name: String,
+    @DrawableRes backgroundImage: Int,
+    rating: Double,
+    ratingAmount: Int,
+    minutes: Int,
+    kcal: Int,
+    @DrawableRes aiAvatar: Int? = null
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(109.dp)
+    ) {
         Card(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(109.dp),
+                .fillMaxSize(),
             shape = RoundedCornerShape(21.dp),
             elevation = CardDefaults.cardElevation(defaultElevation = 0.dp),
             colors = CardDefaults.cardColors(
@@ -64,7 +95,7 @@ fun DishOfTheDay(modifier: Modifier = Modifier) {
                     .clip(RoundedCornerShape(21.dp))
             ) {
                 Image(
-                    painter = painterResource(R.drawable.shrimp_soup_image),
+                    painter = painterResource(backgroundImage),
                     contentDescription = null,
                     contentScale = ContentScale.Crop,
                     modifier = Modifier
@@ -74,13 +105,69 @@ fun DishOfTheDay(modifier: Modifier = Modifier) {
                         .clip(CircleShape)
                 )
                 Text(
-                    text = "Fried Shrimp",
+                    text = name,
                     style = CustomTheme.typography.brightoWander.titleLarge,
                     fontSize = 20.sp,
                     modifier = Modifier.padding(start = 29.dp, top = 36.dp)
                 )
+
+                Row(
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .padding(start = 29.dp, bottom = 4.dp)
+                        .height(22.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    DishDataIcon(
+                        text = "$rating($ratingAmount)",
+                        dishDataIcon = R.drawable.star_ic
+                    )
+                    DishDataIcon(
+                        text = "$minutes min",
+                        dishDataIcon = R.drawable.timer_ic
+                    )
+                    DishDataIcon(
+                        text = "$kcal kcal",
+                        dishDataIcon = R.drawable.flame_ic
+                    )
+                }
             }
         }
+
+        if (aiAvatar != null) {
+            Image(
+                painter = painterResource(aiAvatar),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(49.dp)
+                    .align(Alignment.TopEnd)
+                    .offset(y = (-16).dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun DishDataIcon(
+    modifier: Modifier = Modifier,
+    text: String,
+    @DrawableRes dishDataIcon: Int
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(5.dp),
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(dishDataIcon),
+            contentDescription = null
+        )
+        Text(
+            text = text,
+            style = CustomTheme.typography.gilroy.bodySmall,
+            fontWeight = FontWeight.SemiBold
+        )
     }
 }
 
