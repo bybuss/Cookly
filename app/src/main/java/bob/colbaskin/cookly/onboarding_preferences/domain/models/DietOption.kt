e7067e53ac4372463ea0b4507f8c22dc2fef4cfe
bybuss <kukuhaha729@gmail.com>
2026-03-24 23:31:06 +0300

    added: diet options

--- app/src/main/java/bob/colbaskin/cookly/onboarding_preferences/domain/models/DietOption.kt
+++ app/src/main/java/bob/colbaskin/cookly/onboarding_preferences/domain/models/DietOption.kt
@@ -3,7 +3,18 @@ package bob.colbaskin.cookly.onboarding_preferences.domain.models
 import bob.colbaskin.cookly.R
 
 enum class DietOption(val displayName: String, val imageRes: Int) {
-    Vegan("Веган", R.drawable.smoothie_background),
-    Meat("Мясоед", R.drawable.smoothie_background),
-    Vegetarian("Вегетарианец", R.drawable.smoothie_background)
+    Vegetarian("Вегетарианская", R.drawable.smoothie_background),
+    Ketogenic("Кето", R.drawable.smoothie_background),
+    Paleo("Палео", R.drawable.smoothie_background),
+    LowCarb("Низкоуглеводная", R.drawable.smoothie_background),
+    LowFat("Низкожировая", R.drawable.smoothie_background),
+    Balanced("Сбалансированная", R.drawable.smoothie_background),
+    Mediterranean("Средиземноморская", R.drawable.smoothie_background),
+    JapaneseCuisine("Японская", R.drawable.smoothie_background),
+    ItalianCuisine("Итальянская", R.drawable.smoothie_background),
+    MexicanCuisine("Мексиканская", R.drawable.smoothie_background),
+    IndianCuisine("Индийская", R.drawable.smoothie_background),
+    ThaiCuisine("Тайская", R.drawable.smoothie_background),
+    FrenchCuisine("Французская", R.drawable.smoothie_background),
+    ChineseCuisine("Китайская", R.drawable.smoothie_background)
 }
