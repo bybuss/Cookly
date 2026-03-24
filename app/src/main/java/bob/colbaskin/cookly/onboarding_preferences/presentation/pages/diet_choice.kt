ee89625992e5a245982cb8e308b9d3161bb8a6e3
bybuss <kukuhaha729@gmail.com>
2026-03-25 00:37:57 +0300

    fixed: text overflow

--- app/src/main/java/bob/colbaskin/cookly/onboarding_preferences/presentation/pages/diet_choice.kt
+++ app/src/main/java/bob/colbaskin/cookly/onboarding_preferences/presentation/pages/diet_choice.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -24,7 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import bob.colbaskin.cookly.common.design_system.theme.CustomTheme
@@ -119,7 +118,13 @@ private fun DietCard(
             color = CustomTheme.colors.text,
             modifier = Modifier
                 .align(Alignment.BottomCenter)
-                .padding(bottom = 14.dp)
+                .padding(
+                    start = 8.dp,
+                    end = 8.dp,
+                    bottom = 14.dp
+                ),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
         )
     }
 }
