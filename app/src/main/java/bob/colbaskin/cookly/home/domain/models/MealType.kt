121f7319d7e772285acb39da98e6cd2b0f6156e6
bybuss <kukuhaha729@gmail.com>
2026-03-29 16:47:12 +0300

    added: MealType params

--- app/src/main/java/bob/colbaskin/cookly/home/domain/models/MealType.kt
+++ app/src/main/java/bob/colbaskin/cookly/home/domain/models/MealType.kt
@@ -1,9 +1,31 @@
 package bob.colbaskin.cookly.home.domain.models
 
-enum class MealType {
-    BREAKFAST,
-    LUNCH,
-    AFTERNOON_SNACK,
-    DINNER,
-    SNACK
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import bob.colbaskin.cookly.R
+
+enum class MealType(
+    @param:StringRes val displayNameId: Int,
+    @param:DrawableRes val logoId: Int
+) {
+    BREAKFAST(
+        displayNameId = R.string.breakfast_card_text,
+        logoId = R.drawable.breakfast_logo
+    ),
+    LUNCH(
+        displayNameId = R.string.lunch_card_text,
+        logoId = R.drawable.lunch_logo
+    ),
+    AFTERNOON_SNACK(
+        displayNameId = R.string.afternoon_snack_card_text,
+        logoId = R.drawable.breakfast_logo // TODO: изменить лого на нужное
+    ),
+    DINNER(
+        displayNameId = R.string.dinner_card_text,
+        logoId = R.drawable.dinner_logo
+    ),
+    SNACK(
+        displayNameId = R.string.snack_card_text,
+        logoId = R.drawable.breakfast_logo // TODO: изменить лого на нужное
+    )
 }
