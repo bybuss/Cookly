130bd47e749ccf457824f031ef6f80014c4199f9
bybuss <kukuhaha729@gmail.com>
2026-03-29 16:38:11 +0300

    deleted: unused directives

--- app/src/main/java/bob/colbaskin/cookly/home/presentation/components/TopBarWithSearch.kt
+++ app/src/main/java/bob/colbaskin/cookly/home/presentation/components/TopBarWithSearch.kt
@@ -1,6 +1,5 @@
 package bob.colbaskin.cookly.home.presentation.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -19,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.VisualTransformation
