5c9c1bd19e3a9b331c187a12a03bd7d4e8c5007c
bybuss <kukuhaha729@gmail.com>
2026-04-22 19:27:57 +0300

    changed: old token data store to new

--- app/src/main/java/bob/colbaskin/cookly/auth/data/AuthRepositoryImpl.kt
+++ app/src/main/java/bob/colbaskin/cookly/auth/data/AuthRepositoryImpl.kt
@@ -3,27 +3,26 @@ package bob.colbaskin.cookly.auth.data
 import android.content.Context
 import android.util.Log
 import bob.colbaskin.cookly.auth.data.models.CodeToTokenDTO
-import bob.colbaskin.cookly.auth.domain.local.AuthDataStoreRepository
 import bob.colbaskin.cookly.auth.domain.network.AuthApiService
 import bob.colbaskin.cookly.auth.domain.network.AuthRepository
 import bob.colbaskin.cookly.common.ApiResult
 import bob.colbaskin.cookly.common.user_prefs.data.models.AuthConfig
 import bob.colbaskin.cookly.common.user_prefs.domain.UserPreferencesRepository
 import bob.colbaskin.cookly.common.utils.safeApiCall
-import kotlinx.coroutines.flow.first
+import bob.colbaskin.cookly.di.token.TokenDataStore
 import javax.inject.Inject
 
 private const val TAG = "AuthRepositoryImpl"
 
 class AuthRepositoryImpl @Inject constructor(
     private val context: Context,
-    private val authDataStoreRepository: AuthDataStoreRepository,
+    private val tokenDataStore: TokenDataStore,
     private val authApiService: AuthApiService,
     private val userPreferencesRepository: UserPreferencesRepository
 ) : AuthRepository {
 
     override suspend fun isLoggedIn(): Boolean {
-        val token = authDataStoreRepository.getToken().first()
+        val token = tokenDataStore.getAccessToken()
         return !token.isNullOrEmpty()
     }
 
